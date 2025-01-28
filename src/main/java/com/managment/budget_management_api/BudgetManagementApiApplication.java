bb1f899d43f399c0f2a56fbf091aa48c92e796fd
Leon-ER <leonerdesi2004@gmail.com>
2025-01-28 13:23:32 -0500

    Initial commit

---
+++ src/main/java/com/managment/budget_management_api/BudgetManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.managment.budget_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetManagementApiApplication.class, args);
+	}
+
+}
