107357aeba47141e24453158468291f0bfd2072a
Leon-ER <leonerdesi2004@gmail.com>
2025-01-30 13:59:46 -0500

    Added summary for transactions

--- src/main/java/com/managment/budget_management_api/Repository/UserRepository.java
+++ src/main/java/com/managment/budget_management_api/Repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.managment.budget_management_api.Repository;
 
+import com.managment.budget_management_api.Model.Transaction;
 import com.managment.budget_management_api.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/managment/budget_management_api/Service/ITransactionService.java
+++ src/main/java/com/managment/budget_management_api/Service/ITransactionService.java
@@ -1,6 +1,7 @@
 package com.managment.budget_management_api.Service;
 
 import com.managment.budget_management_api.Model.Transaction;
+import com.managment.budget_management_api.Model.TransactionSummary;
 
 import java.util.Optional;
 
@@ -12,5 +13,6 @@ public interface ITransactionService {
 
     Optional<Transaction> findById(Integer transactionID);
 
+    Optional<TransactionSummary> getSummary(Integer userId);
     void deleteById(Integer transactionID);
 }
